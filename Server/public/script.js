73e369940b5ed764226943776b6c7e64a4a4347b
GabiZar <GabiZar@proton.me>
2026-05-20 10:18:08 +0200

    Add joystick controle with keyboard and button controle priority over joystick controle

--- Server/public/script.js
+++ Server/public/script.js
@@ -43,35 +43,69 @@ connect();
 
 let lastSend = 0;
 let isStopped = false;
-const delay = 5000;
+let lastDirection = { type: "keyboard", command: "STOP" };
 
 document.addEventListener("keydown", (event) => {
     console.log("Key pressed:", event.key);
 
     if (event.key === "z" || event.key === "ArrowUp") {
-        move("FORWARD");
+        lastDirection = { type: "keyboard", command: "FORWARD" };
     } else if (event.key === "s" || event.key === "ArrowDown") {
-        move("BACKWARD");
+        lastDirection = { type: "keyboard", command: "BACKWARD" };
     } else if (event.key === "q" || event.key === "ArrowLeft") {
-        move("LEFT");
+        lastDirection = { type: "keyboard", command: "LEFT" };
     } else if (event.key === "d" || event.key === "ArrowRight") {
-        move("RIGHT");
+        lastDirection = { type: "keyboard", command: "RIGHT" };
     }
 });
 
 document.addEventListener("keyup", (event) => {
     if (!isStopped) {
-        sendCmd("STOP");
-        lastSend = 0;
-        isStopped = true;
+        lastDirection = { type: "keyboard", command: "STOP" };
     }
 });
 
 function move(direction) {
     const now = Date.now();
-    if (now - lastSend >= delay) {
-        lastSend = now;
+    if (now - lastSend >= 5000) {
+        lastSend = direction === "STOP" ? 0 : now;
         sendCmd(direction);
-        isStopped = false;
+        isStopped = direction === "STOP" ? true : false;
     }
+}
+
+var movementJoystick = new JoyStick("movementJoystick", {}, (stickData) => {
+    console.log(stickData.cardinalDirection);
+
+    if (lastDirection.type === "keyboard" && lastDirection.command !== "STOP") {
+        return;
+    }
+    if (stickData.cardinalDirection === "N") {
+        lastDirection = { type: "joystick", command: "FORWARD" };
+    } else if (stickData.cardinalDirection === "S") {
+        lastDirection = { type: "joystick", command: "BACKWARD" };
+    } else if (stickData.cardinalDirection === "W") {
+        lastDirection = { type: "joystick", command: "LEFT" };
+    } else if (stickData.cardinalDirection === "E") {
+        lastDirection = { type: "joystick", command: "RIGHT" };
+    } else if (stickData.cardinalDirection === "C") {
+        lastDirection = { type: "joystick", command: "STOP" };
+    }
+});
+
+async function loop() {
+    while (true) {
+        console.log(lastDirection.command);
+        move(lastDirection.command);
+
+        await delay(10);
+    }
+}
+
+loop();
+
+function delay(ms) {
+    move(lastDirection.command);
+
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
